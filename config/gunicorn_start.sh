ce51c88b41fcab0551643b730669c7f589c01a6e
Ubuntu <ubuntu@ip-172-31-18-169.ap-southeast-1.compute.internal>
2016-02-08 21:30:39 +0000

    gunicorn setup

--- config/gunicorn_start.sh
+++ config/gunicorn_start.sh
@@ -1,23 +1,51 @@
+#!/bin/sh
+
+#NAME="ronak_blog"
+#DJANGODIR=/home/django_blog
+#SOCKFILE=/tmp/foofish.sock
+#USER=root
+#GROUP=root
+#NUM_WORKERS=3
+#DJANGO_SETTINGS_MODULE=settings.prod
+#DJANGO_WSGI_MODULE=wsgi
+
+#echo "Starting $NAME as `whoami`"
+
+#cd $DJANGODIR
+#source /root/envs/django_blog/bin/activate
+#export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+#export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+#RUNDIR=$(dirname $SOCKFILE)
+#test -d $RUNDIR || mkdir -p $RUNDIR
+
+#exec /root/envs/django_blog/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+#--name $NAME \
+#--workers $NUM_WORKERS \
+#--user=$USER --group=$GROUP \
+#--log-level=error \
+#--bind=unix:$SOCKFILE
+
 NAME="ronak_blog"
-DJANGODIR=/home/django_blog
-SOCKFILE=/tmp/foofish.sock
-USER=root
-GROUP=root
+DJANGODIR=/home/ubuntu/websites/simplecode.in
+SOCKFILE=/home/ubuntu/dblog.sock
+USER=ubuntu
+GROUP=ubuntu
 NUM_WORKERS=3
-DJANGO_SETTINGS_MODULE=settings.prod
-DJANGO_WSGI_MODULE=wsgi
+DJANGO_SETTINGS_MODULE=django_blog.settings.dev
+DJANGO_WSGI_MODULE=django_blog.wsgi
 
 echo "Starting $NAME as `whoami`"
 
 cd $DJANGODIR
-source /root/envs/django_blog/bin/activate
+source /home/ubuntu/virtenvs/simplecode/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
-exec /root/envs/django_blog/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
 --name $NAME \
 --workers $NUM_WORKERS \
 --user=$USER --group=$GROUP \
--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+#import os
 # from os.path import abspath, dirname
 # from sys import path
 
@@ -16,13 +16,30 @@ import os
 
 # print('Project name ==> {{ project_name }}')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.dev")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.dev")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
 
 
 # from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.dev")
 # application = get_wsgi_application()
+import os
+from os.path import abspath, dirname
+from sys import path
+
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+print(SITE_ROOT)
+print(path)
+
+path.append(SITE_ROOT)
+
+print(path)
+# print('Project name ==> {{ project_name }}')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.dev")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
