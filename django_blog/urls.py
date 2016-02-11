a764a9270f322db903a1abcd617131aa39a6bacc
Ronak Mundra <ronakmundra123@gmail.com>
2016-02-11 19:39:28 +0530

    seo related changes

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -13,14 +13,13 @@ sitemaps = {
 
 urlpatterns = [
         url(r'^about$', views.about, name='about'),
-        # url(r'^blog/', include('apps.blog.urls', namespace='blog')),
         url(r'^', include('apps.blog.urls', namespace='blog')),
         url(r'^wedding/', include('apps.wedding.urls', namespace='wedding')),
         url(r'^admin/', include(admin.site.urls)),
         url(r'404', views.not_found),
         url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
            name='django.contrib.sitemaps.views.sitemap'),
-        url(r'^robots.txt$',
+        url(r'^robots.txt/$',
            lambda r: HttpResponse("User-agent: *\nDisallow: /admin/\nSitemap: <http://simplecode.in/sitemap.xml>", content_type="text/plain")),
         url(r'^rss/', views.LatestPosts(), name='feeds')
       ]
